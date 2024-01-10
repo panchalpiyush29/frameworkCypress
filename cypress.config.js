8ca00d2a5296941fdd29dee56330a5f13641fe01
Piyush Panchal <panchalpiyush29@gmail.com>
2024-01-11 12:04:39 +1300

    added environments
    added environments

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,10 @@
-const {defineConfig} = require("cypress");
+const {defineConfig} = require("cypress")
+const dotenv = require("dotenv");
+
+dotenv.config({path: `.env.staging`})
 
 module.exports = defineConfig({
+
     video: false,
     videosFolder: 'cypress/videos',
     screenshotOnRunFailure: true,
@@ -8,6 +12,7 @@ module.exports = defineConfig({
     trashAssetsBeforeRuns: true,
     viewportWidth: 1536,
     viewportHeight: 960,
+
     reporter: 'cypress-mochawesome-reporter',
     reporterOptions: {
         charts: true,
@@ -18,12 +23,15 @@ module.exports = defineConfig({
     },
 
     e2e: {
-        baseUrl: 'https://www.saucedemo.com/',
+
         defaultCommandTimeout: 5000,
         retries: 1,
         chromeWebSecurity: false,
         setupNodeEvents(on, config) {
             require('cypress-mochawesome-reporter/plugin')(on);
+            config.baseUrl = process.env.BASE_URL;
+
+            return config;
         },
     },
 });
